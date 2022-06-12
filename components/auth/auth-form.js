b452bee46e0f2748e9eb71d796961e6be7acf92c
felix fedronic <fedronicfelix@gmail.com>
2022-06-13 01:33:36 +0200

    Sending signup from the frontend DONE

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,33 +1,89 @@
-import { useState } from 'react';
-import classes from './auth-form.module.css';
+import { useState, useRef } from "react";
+import classes from "./auth-form.module.css";
+
+/**
+ * It sends a POST request to the /api/auth/signup endpoint with the email and password as the body of
+ * the request. If the response is not ok, it throws an error. Otherwise, it returns the data from the
+ * response
+ * @param email - The email address of the user.
+ * @param password - "12345678"
+ * @returns The data object is being returned.
+ */
+async function createUser(email, password) {
+  const response = await fetch("/api/auth/signup", {
+    method: "POST",
+    body: JSON.stringify({ email, password }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(response.message || "Something went wrong!");
+  }
+
+  const data = await response.json();
+
+  return data;
+}
 
 function AuthForm() {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
   const [isLogin, setIsLogin] = useState(true);
 
+  /**
+   * The function switchAuthModeHandler() is a function that takes no arguments and returns a function
+   * that takes one argument, prevState, and returns the opposite of prevState.
+   */
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
   }
 
+  /**
+   * The function is called when the user clicks the submit button. It prevents the default action of
+   * the submit button, which is to reload the page. It then gets the values of the email and password
+   * inputs and passes them to the createUser function. It then logs the result of the createUser
+   * function to the console.
+   * @param event - The event object that is passed to the event handler.
+   */
+  async function onSubmitHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    const result = await createUser(enteredEmail, enteredPassword);
+
+    console.log(result);
+  }
+
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={onSubmitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          <button>{isLogin ? "Login" : "Create Account"}</button>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -2,27 +2,29 @@ import { connectToDatabase, insertNewUser } from "../../../lib/db";
 import { hashPassword } from "../../../lib/auth";
 
 async function handler(req, res) {
-  const data = req.body;
+  if (req.method === "POST") {
+    const data = req.body;
 
-  const { email, password } = data;
+    const { email, password } = data;
 
-  const client = await connectToDatabase();
+    const client = await connectToDatabase();
 
-  try {
-    const hashedPassword = await hashPassword(password);
+    try {
+      const hashedPassword = await hashPassword(password);
 
-    const result = await insertNewUser(client, "users", {
-      email: email,
-      password: hashedPassword,
-    });
+      const result = await insertNewUser(client, "users", {
+        email: email,
+        password: hashedPassword,
+      });
 
-    res.status(201).json({ message: result });
+      res.status(201).json({ message: "created user!" });
 
-    client.close();
-  } catch (error) {
-    res.status(400).json({ error: "invalid input" });
+      client.close();
+    } catch (error) {
+      res.status(400).json({ message: "invalid input" });
 
-    client.close();
+      client.close();
+    }
   }
 }
 
