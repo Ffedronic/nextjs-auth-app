902ca3179b13edbf835684a2248427ad10ab9c06
felix fedronic <fedronicfelix@gmail.com>
2022-06-13 15:20:47 +0200

    adding credentials auth provider user login DONE

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,40 @@
+import nextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import { verifyPassword } from "../../../lib/auth";
+import { connectToDatabase, isExistingUser } from "../../../lib/db";
+
+export default nextAuth({
+  session: {
+    jwt: true,
+  },
+  providers: [
+    Providers.Credentials({
+      async authorize(credentials) {
+        const client = await connectToDatabase();
+
+        const existingUser = await isExistingUser(client, "users", {
+          email: credentials.email,
+        });
+
+        if (!existingUser) {
+          client.close();
+          throw new Error("No user found!");
+        }
+
+        const isValid = await verifyPassword(
+          credentials.password,
+          existingUser.password
+        );
+
+        if (!isValid) {
+          client.close();
+          throw new Error("Could not log you in!");
+        }
+
+        client.close();
+
+        return { email: existingUser.email };
+      },
+    }),
+  ],
+});
